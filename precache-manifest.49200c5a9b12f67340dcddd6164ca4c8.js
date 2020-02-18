69112c754c672a431aa79d6cd0194e651098b6ae
Pierre KORE <pierre.kore@infeeny.com>
2020-02-18 15:31:04 +0100

    Updates

---
+++ precache-manifest.49200c5a9b12f67340dcddd6164ca4c8.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13604f04162717aa749cc40f1d8a74b",
+    "url": "/Belladone/index.html"
+  },
+  {
+    "revision": "5b7f9ffcf4a223246056",
+    "url": "/Belladone/static/css/main.0a628a0a.chunk.css"
+  },
+  {
+    "revision": "65729f14748f6a60d105",
+    "url": "/Belladone/static/js/2.69c8d57e.chunk.js"
+  },
+  {
+    "revision": "5b7f9ffcf4a223246056",
+    "url": "/Belladone/static/js/main.4b7e18f3.chunk.js"
+  },
+  {
+    "revision": "a2587b2b48f777814e66",
+    "url": "/Belladone/static/js/runtime~main.a5cab59d.js"
+  },
+  {
+    "revision": "65d2bcf2623d6990b812c4e4ef0ce154",
+    "url": "/Belladone/static/media/Logobelladone.65d2bcf2.png"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/Belladone/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "76e1d23ddc84dd97d3a27ccd76acaffb",
+    "url": "/Belladone/static/media/belladone01-06-19.76e1d23d.jpg"
+  },
+  {
+    "revision": "3e09c0571c4d3b73481ed07b2c1e6a4b",
+    "url": "/Belladone/static/media/belladone02-05-19.3e09c057.jpg"
+  },
+  {
+    "revision": "253409b3a13e01a72281447f0c964cac",
+    "url": "/Belladone/static/media/belladone23-05-19.253409b3.jpg"
+  },
+  {
+    "revision": "04f4b2be3765a61943075cc8b7311ed8",
+    "url": "/Belladone/static/media/belladone29-05-19.04f4b2be.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/Belladone/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/Belladone/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/Belladone/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/Belladone/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/Belladone/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/Belladone/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/Belladone/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/Belladone/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/Belladone/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "8946dfb3c0a5f095e776f2ae9a7ec84b",
+    "url": "/Belladone/static/media/logoUp.8946dfb3.png"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/Belladone/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/Belladone/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "22b1fadbfc3362b772a753650c0e04a5",
+    "url": "/Belladone/static/media/party-down.22b1fadb.jpg"
+  },
+  {
+    "revision": "7f16ee03a6e6942875e357b40d28fa91",
+    "url": "/Belladone/static/media/party-up.7f16ee03.jpg"
+  },
+  {
+    "revision": "f7d78c80c3808f9011b4391a7ef88a7e",
+    "url": "/Belladone/static/media/party.f7d78c80.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/Belladone/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/Belladone/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/Belladone/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/Belladone/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/Belladone/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/Belladone/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/Belladone/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/Belladone/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
